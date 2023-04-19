a2ac6dfc90dff9782eccb811674d990eb7ce0ad9
Nikola Milosavljevic <nikola.milosavljevic.enem@gmail.com>
2023-04-19 15:52:52 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "testingxd-40022",
-  url: "https://testingxd-40022.botics.co",
-  api: "https://testingxd-40022.botics.co/api/v1"
-}
+  "name": "testingxd-40022",
+  "url": "https://testingxd-40022.botics.co",
+  "api": "https://testingxd-40022.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
